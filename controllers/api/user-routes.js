ca2c30f49306831f84855216d93523795bce1552
Erin Holmes <e.holmes09@yahoo.com>
2021-08-13 16:02:36 -0500

    create put and delete routes for users

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require("../../models");
 
+// get all users
 router.get("/", (req, res) => {
   User.findAll({
     attributes: { exclude: ["password"] }
@@ -12,6 +13,7 @@ router.get("/", (req, res) => {
     });
 });
 
+// get users by id
 router.get("/:id", (req, res) => {
   User.findOne({
     attributes: { exclude: ["password"] },
@@ -38,6 +40,7 @@ router.get("/:id", (req, res) => {
   });
 });
 
+// create new user 
 router.post("/", (req, res) => {
   User.create({
     username: req.body.username,
@@ -53,36 +56,73 @@ router.post("/", (req, res) => {
     });
 });
 
-router.post("/login", (req, res) => {
-  User.findOne({
+// user login route
+// router.post("/login", (req, res) => {
+//   User.findOne({
+//     where: {
+//       email: req.body.email
+//     }
+//   })
+//   .then(dbUserData => {
+//     if (!dbUserData) {
+//       res.status(400).json({ message: "No user with that email address!" });
+//       return;
+//     }
+//     const validPassword = dbUserData.checkPassword(req.body.password);
+//     if (!validPassword) {
+//       res.status(400).json({ message: "Incorrect password!" });
+//       return;
+//     }
+//     req.session.save(() => {
+//       req.session.user_id = dbUserData.id;
+//       req.session.username = dbUserData.username;
+//       req.session.loggedIn = true;
+
+//       res.json({ user: dbUserData, message: "You are now logged in!" });
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+//   });
+// });
+
+// update user 
+router.put("/:id", (req, res) => {
+  User.update(req.body, {
+    individualHooks: true,
     where: {
-      email: req.body.email
+      id: req.params.id
     }
   })
-  .then(dbUserData => {
-    if (!dbUserData) {
-      res.status(400).json({ message: "No user with that email address!" });
-      return;
-    }
-    const validPassword = dbUserData.checkPassword(req.body.password);
-    if (!validPassword) {
-      res.status(400).json({ message: "Incorrect password!" });
-      return;
-    }
-    req.session.save(() => {
-      req.session.user_id = dbUserData.id;
-      req.session.username = dbUserData.username;
-      req.session.loggedIn = true;
-
-      res.json({ user: dbUserData, message: "You are now logged in!" });
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id"});
+      }
+      res.json(dbUserData);
     })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
-  });
 });
-  
 
+router.delete("/:id", (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id"});
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+})
 
 module.exports = router;
