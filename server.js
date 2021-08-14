d849592b6bbf0a488a7d8b18317c30dc5dfc0624
Erin Holmes <e.holmes09@yahoo.com>
2021-08-14 14:51:40 -0500

    add helper fn to format date and set up on server

--- server.js
+++ server.js
@@ -22,7 +22,10 @@ const sess = {
 
 app.use(session(sess));
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+const helpers = require("./utils/helpers");
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
